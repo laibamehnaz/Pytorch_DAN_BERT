bc933228fbed0f21a5534edfaf9c1e0b6e3f8161
laibamehnaz <36405283+laibamehnaz@users.noreply.github.com>
2020-11-09 00:06:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -13,40 +13,55 @@ import utils
 import models
 import params
 import train, test
-from transformers import BertModel, BertConfig, BertTokenizer, BertTokenizerFast
+from transformers import BertModel, BertConfig, BertTokenizer, BertTokenizerFast, AdamW, get_linear_schedule_with_warmup
 
 
 ###BERT model instead of the Extractor
 # create the BERTConfig, BERTTokenizer, and BERTModel 
 model_name = "bert-base-uncased"
-config = BertConfig.from_pretrained(model_name, output_hidden_states=True)
+config = BertConfig.from_pretrained(model_name, output_hidden_states=True,  return_dict=True)
 tokenizer = BertTokenizerFast.from_pretrained(model_name, do_lower_case=True)
 bert = BertModel.from_pretrained(model_name, config=config)
 
-src_train_dataloader = utils.get_train_loader(tokenizer)
-src_test_dataloader =utils.get_test_loader(tokenizer)
-tgt_train_dataloader = utils.get_train_loader(tokenizer)
-tgt_test_dataloader = utils.get_test_loader(tokenizer)
+src_train_dataloader = utils.get_train_loader('/content/drive/My Drive/Data_summarization/pytorch_DAN/data/books.csv',tokenizer)
+src_test_dataloader =utils.get_test_loader('/content/drive/My Drive/Data_summarization/pytorch_DAN/data/books.csv',tokenizer)
+tgt_train_dataloader = utils.get_train_loader('/content/drive/My Drive/Data_summarization/pytorch_DAN/data/dvd.csv',tokenizer)
+tgt_test_dataloader = utils.get_test_loader('/content/drive/My Drive/Data_summarization/pytorch_DAN/data/dvd.csv',tokenizer)
 
 
 
 common_net = bert
-src_net = BertForSequenceClassification(config, common_net)
-tgt_net = BertForSequenceClassification(config, common_net)
+src_net = models.BertForSequenceClassification(config, common_net)
+tgt_net = models.BertForSequenceClassification(config, common_net)
+
 
 
 src_dataiter = iter(src_train_dataloader)
 tgt_dataiter = iter(tgt_train_dataloader)
-"""
-src_imgs, src_labels = next(src_dataiter)
-tgt_imgs, tgt_labels = next(tgt_dataiter)
 
-src_imgs_show = src_imgs[:4]
-tgt_imgs_show = tgt_imgs[:4]
+src_text, src_input_ids, src_attention_mask, src_targets = next(src_dataiter)
+tgt_text, tgt_input_ids, tgt_attention_mask, tg_targets = next(tgt_dataiter)
+#tgt_imgs, tgt_labels = next(tgt_dataiter)
+
+#print(src_input_ids)
+#print(type(next(src_dataiter)))
+#print(len(next(src_dataiter)))
+#print(next(src_dataiter)[src_input_ids])
+
+#print(next(src_dataiter))
+#print(next(tgt_dataiter))
+
+src_input_tensor=next(src_dataiter)[src_input_ids]
+tgt_input_tensor=next(tgt_dataiter)[tgt_input_ids]
+
+src_imgs_show = src_input_tensor
+tgt_imgs_show = tgt_input_tensor
 
 utils.imshow(vutils.make_grid(src_imgs_show))
 utils.imshow(vutils.make_grid(tgt_imgs_show))
-"""
+
+
+
 train_hist = {}
 train_hist['Total_loss'] = []
 train_hist['Class_loss'] = []
@@ -61,12 +76,17 @@ if params.use_gpu:
     src_net.cuda()
     tgt_net.cuda()
 
-##To be fixed if needed to visualise
-"""
-src_features = common_net(Variable(src_imgs.expand(src_imgs.shape[0], 3, 28, 28).cuda()))
-tgt_features = common_net(Variable(tgt_imgs.expand(tgt_imgs.shape[0], 3, 28, 28).cuda()))
-src_features = src_features.cpu().data.numpy()
-tgt_features = tgt_features.cpu().data.numpy()
+src_features = common_net(src_input_tensor.cuda())
+tgt_features = common_net(tgt_input_tensor.cuda())
+
+
+print("MODEL OUTPUTS:")
+print(src_features['last_hidden_state'][0].size())
+##Need to look into what the model returns to be able to plot
+
+src_features = src_features['last_hidden_state'][0].cpu().data.numpy()
+tgt_features = tgt_features['last_hidden_state'][0].cpu().data.numpy()
+
 src_features = TSNE(n_components= 2).fit_transform(src_features)
 tgt_features = TSNE(n_components= 2).fit_transform(tgt_features)
 
@@ -74,10 +94,43 @@ plt.scatter(src_features[:, 0], src_features[:, 1], color = 'r')
 plt.scatter(tgt_features[:, 0], tgt_features[:, 1], color = 'b')
 plt.title('Non-adapted')
 pylab.show()
-"""
+
+###Trying to remove the optimizer 
 optimizer = optim.SGD([{'params': common_net.parameters()},
                        {'params': src_net.parameters()},
                        {'params': tgt_net.parameters()}], lr= params.lr, momentum= params.momentum)
+"""
+no_decay = ["bias", "LayerNorm.weight"]
+optimizer_grouped_parameters = [
+                {
+                "params": [p for n, p in common_net.named_parameters() if not any(nd in n for nd in no_decay)],
+                 "weight_decay": 0.0,
+                },
+                {
+                    "params": [p for n, p in common_net.named_parameters() if any(nd in n for nd in no_decay)],
+                    "weight_decay": 0.0,
+                },
+                {
+                    "params": [p for n, p in src_net.named_parameters() if any(nd in n for nd in no_decay)],
+                    "weight_decay": 0.0,
+                },
+                {
+                    "params": [p for n, p in tgt_net.named_parameters() if any(nd in n for nd in no_decay)],
+                    "weight_decay": 0.0,
+                }
+            ]
+optimizer = AdamW(
+                optimizer_grouped_parameters,
+                lr=3e-4,
+                betas=(0.9, 0.99),
+                eps=1e-8,
+            )
+lr_scheduler = get_linear_schedule_with_warmup(
+                optimizer, num_warmup_steps=10, num_training_steps=100
+            )
+
+
+"""
 
 criterion = nn.CrossEntropyLoss()
 
