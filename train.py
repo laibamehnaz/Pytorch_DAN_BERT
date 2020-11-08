ca117828c8082039577383ff969b94b344b2375f
laibamehnaz <36405283+laibamehnaz@users.noreply.github.com>
2020-11-09 00:10:33 +0400

    Update train.py

--- train.py
+++ train.py
@@ -18,15 +18,29 @@ def train(common_net, src_net, tgt_net, optimizer, criterion, epoch,
 
     source_iter = iter(source_dataloader)
     target_iter = iter(target_dataloader)
-
+    
     for batch_idx in range(min(len(source_dataloader), len(target_dataloader))):
         # get data
-        sdata = next(source_iter)
-        tdata = next(target_iter)
+        #sdata = next(source_iter)
+        #data = next(target_iter)
+        src_text, src_input_ids, src_attention_mask, src_targets = next(source_iter)
+        tgt_text, tgt_input_ids, tgt_attention_mask, tgt_targets = next(target_iter)
+
+
+        input1=next(source_iter)[src_input_ids]
+        attention_mask1=next(source_iter)[ src_attention_mask]
+        label1=next(source_iter)[src_targets]
+
+        input2= next(target_iter)[tgt_input_ids]
+        attention_mask2=next(target_iter)[tgt_attention_mask]
+        label2=next(target_iter)[tgt_targets]
 
         # prepare the data
-        input1, label1 = sdata
-        input2, label2 = tdata
+        
+        #input1, label1 = sdata
+        #input2, label2 = tdata
+        
+        """
         if params.use_gpu:
             input1, label1 = Variable(input1.cuda()), Variable(label1.cuda())
             input2, label2 = Variable(input2.cuda()), Variable(label2.cuda())
@@ -34,14 +48,23 @@ def train(common_net, src_net, tgt_net, optimizer, criterion, epoch,
             input1, label1 = Variable(input1), Variable(label1)
             input2, label2 = Variable(input2), Variable(label2)
 
+        """
+
+
+        input1, attention_mask1, label1 = input1.cuda(), attention_mask1.cuda(), label1.cuda()
+        input2, attention_mask2, label2 = input2.cuda(), attention_mask2.cuda(), label2.cuda()
+
         optimizer.zero_grad()
 
         #input1 = input1.expand(input1.shape[0], 3, 28, 28)
         
         input = torch.cat((input1, input2), 0)
-        common_feature = common_net(input)
+        attention_mask = torch.cat((attention_mask1, attention_mask2), 0)
+        common_feature = common_net(input, attention_mask)
 
-        src_feature, tgt_feature = torch.split(common_feature, int(params.batch_size))
+        src_feature, tgt_feature = torch.split(common_feature['pooler_output'], int(2))
+
+        #print(len(torch.split(common_feature['pooler_output'], int(2))))
 
         src_output = src_net(src_feature)
         tgt_output = tgt_net(tgt_feature)
@@ -56,13 +79,17 @@ def train(common_net, src_net, tgt_net, optimizer, criterion, epoch,
         optimizer.step()
         step = epoch * len(target_dataloader) + batch_idx
 
+        #print('Loss.data:')
+        #print(loss.size())
+        #print(mmd_loss.data)
+
 
         if (batch_idx + 1) % params.plot_iter == 0:
             print('[{}/{} ({:.0f}%)]\tLoss: {:.6f}\tClass Loss: {:.6f}\tMMD Loss: {:.6f}'.format(
                 batch_idx * len(input2), len(target_dataloader.dataset),
-                100. * batch_idx / len(target_dataloader), loss.data[0], class_loss.data[0],
-                mmd_loss.data[0]
+                100. * batch_idx / len(target_dataloader), loss.data, class_loss.data,
+                mmd_loss.data
             ))
-            train_hist['Total_loss'].append(loss.cpu().data[0])
-            train_hist['Class_loss'].append(class_loss.cpu().data[0])
-            train_hist['MMD_loss'].append(mmd_loss.cpu().data[0])
+            train_hist['Total_loss'].append(loss.cpu().data)
+            train_hist['Class_loss'].append(class_loss.cpu().data)
+            train_hist['MMD_loss'].append(mmd_loss.cpu().data)
